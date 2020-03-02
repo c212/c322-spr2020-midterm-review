ae3274716cdac87cbfbbf149ffe16cf96024d1c2
c212 <40395648+c212@users.noreply.github.com>
2020-03-02 00:55:29 -0500

    Update Client.java

--- factory method/Client.java
+++ factory method/Client.java
@@ -1,17 +1,6 @@
-/*
- *  Java Design Pattern Essentials - Second Edition, by Tony Bevis
- *  Copyright 2012, Ability First Limited
- *
- *  This source code is provided to accompany the book and is provided AS-IS without warranty of any kind.
- *  It is intended for educational and illustrative purposes only, and may not be re-published
- *  without the express written permission of the publisher.
- */
 package chapter04;
-
 import chapter01.*;
-
 public class Client {
-    
     public static void main(String[] args) {
         // I want an economical car, coloured blue...
         VehicleFactory carFactory = new CarFactory();
@@ -19,14 +8,12 @@ public class Client {
                 (VehicleFactory.DrivingStyle.ECONOMICAL,
                  Vehicle.Colour.BLUE);
         System.out.println(car);
-        
         // I am a "white van man"...
         VehicleFactory vanFactory = new VanFactory();
         Vehicle van = vanFactory.build
                 (VehicleFactory.DrivingStyle.POWERFUL,
                  Vehicle.Colour.WHITE);
         System.out.println(van);
-        
         // Create a red sports car...
         Vehicle sporty = VehicleFactory.make
                                 (VehicleFactory.Category.CAR,
@@ -34,5 +21,4 @@ public class Client {
                                  Vehicle.Colour.BLUE);
         System.out.println(sporty);
     }
-    
 }
