74615ed2d1e35a3a3840ddf612f357ac2af822bd
c212 <40395648+c212@users.noreply.github.com>
2020-03-02 01:00:24 -0500

    Update GearboxFactory.java

--- simple factory/GearboxFactory.java
+++ simple factory/GearboxFactory.java
@@ -1,14 +1,10 @@
 public class GearboxFactory {
-
     public enum Type {AUTOMATIC, MANUAL};
-
     public static Gearbox create(Type type) {
         if (type == Type.AUTOMATIC) {
             return new AutomaticGearbox();
-
         } else {
             return new ManualGearbox();
         }
     }
-
 }
