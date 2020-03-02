057e3c97905e2ac5a59fc8c92212974b8da0fe8b
c212 <40395648+c212@users.noreply.github.com>
2020-03-01 23:18:38 -0500

    Add files via upload

---
+++ simple factory/GearboxFactory.java
@@ -0,0 +1,14 @@
+public class GearboxFactory {
+
+    public enum Type {AUTOMATIC, MANUAL};
+
+    public static Gearbox create(Type type) {
+        if (type == Type.AUTOMATIC) {
+            return new AutomaticGearbox();
+
+        } else {
+            return new ManualGearbox();
+        }
+    }
+
+}
